0d16a951a2111ab71f32a9cbfa73151612d99ab6
kench1r0 <nattawut.eleven@gmail.com>
2025-12-30 19:27:51 +0700

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Highlight from './components/Highlight'
 import Contents from './components/Contents'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Highlight />
         <Contents />
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer-section">
+      <div className="footer-content">
+        <p className="footer-text">© 2025 Kenzchiro. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
