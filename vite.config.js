91ba76056c0ce2e771bc8afdbd305916492cd73f
kench1r0 <nattawut.eleven@gmail.com>
2026-01-27 18:25:10 +0700

    chore: remove comment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const port = Number(env.PORT || env.VITE_PORT || 3000)
