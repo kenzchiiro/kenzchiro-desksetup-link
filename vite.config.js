83054bb83a3632d6d2e5b5525feab1587fd3b19e
kench1r0 <nattawut.eleven@gmail.com>
2026-01-27 18:22:44 +0700

    feat: update config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
   plugins: [
     react(),
     tailwindcss(),
